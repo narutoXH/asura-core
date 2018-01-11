10a493c56601460acee5a6a010a91015e7b1ac11
niefei <fei.nie@56qq.com>
2018-01-11 23:47:24 +0800

    优化工程结构，asura-core提供必要的、公共的工具方法

--- src/main/java/org/asura/core/util/JsonUtil.java
+++ src/main/java/org/asura/core/util/JsonUtil.java
@@ -1,7 +1,13 @@
 package org.asura.core.util;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
+import com.jayway.jsonpath.Filter;
+import com.jayway.jsonpath.JsonPath;
 
 public class JsonUtil {
 
@@ -16,5 +22,22 @@ public class JsonUtil {
 	public static <T> T json2Object(String json, T type) {
 		return JSONObject.parseObject(json, new TypeReference<T>() {
 		});
+	}
+	
+	public static String[] getValues(String content, String path) {
+		List<?> objs = (List<?>) JsonPath.read(content, path, new Filter[0]);
+
+		List<String> list = new ArrayList<>();
+		for (Iterator<?> its = objs.iterator(); its.hasNext();) {
+			Object obj = its.next();
+			list.add(obj.toString());
+		}
+
+		return list.toArray(new String[0]);
+	}
+
+	public static String getValue(String content, String path) {
+		Object obj = JsonPath.read(content, path, new Filter[0]);
+		return obj.toString();
 	}
 }
